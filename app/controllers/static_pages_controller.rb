ab7235dabf93eb557653a536a9f5d9280b5f3074
harluz <harluz06@gmail.com>
2020-08-01 15:43:39 +0900

    Creation of static page and modification of development environment2

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,25 +1,19 @@
+# frozen_string_literal: true
+
 class StaticPagesController < ApplicationController
-  def home
-  end
+  def home; end
 
-  def log_in
-  end
+  def log_in; end
 
-  def log_out
-  end
+  def log_out; end
 
-  def about
-  end
+  def about; end
 
-  def profile
-  end
+  def profile; end
 
-  def my_list
-  end
+  def my_list; end
 
-  def suggest
-  end
+  def suggest; end
 
-  def seek
-  end
+  def seek; end
 end
