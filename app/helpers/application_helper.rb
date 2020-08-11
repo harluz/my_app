f2188e766c75d5c36e5882b327757d10338b2475
harluz <harluz06@gmail.com>
2020-08-11 15:29:44 +0900

    Corrected rubocop -a

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
-  def full_title(page_title = '')  # full_titleメソッドを定義
+  def full_title(page_title = '') # full_titleメソッドを定義
     base_title = 'Theme words'
     if page_title.blank?
-      base_title  # トップページはタイトル「Theme words」
+      base_title # トップページはタイトル「Theme words」
     else
       "#{page_title} - #{base_title}" # トップ以外のページはタイトル「profile - Theme words」（例）
     end
