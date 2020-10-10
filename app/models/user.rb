3545a71787ca3ebe5216f4cc3a4e66cc37a6bdca
harluz <harluz06@gmail.com>
2020-10-10 12:11:51 +0900

    Add profile image

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,5 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6 }
   has_secure_password
+  mount_uploader :image, ImageUploader
 end
