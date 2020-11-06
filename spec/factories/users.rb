46fa805b9f6494e7c833149e52002a506b792c56
harluz <harluz06@gmail.com>
2020-11-06 22:34:59 +0900

    Add factory user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -9,4 +9,12 @@ FactoryBot.define do
     password { 'foobar' }
     password_confirmation { 'foobar' }
   end
+
+  factory :invalid_user, class: User do
+    name { Faker::Name.name }
+    sequence(:email) { |n| "example#{n}@example.com" }
+    # image { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/test.jpg')) }
+    password { 'foobar' }
+    password_confirmation { 'foobaz' }
+  end
 end
