5dc4e6b907d60d4fd40a9c7d46a4a620673cc4b1
harluz <harluz06@gmail.com>
2020-12-06 11:42:16 +0900

    Add evolving login mechanism

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,12 +7,12 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    # user && user.authenticate(params[:session][:password]) (rubocopより修正)
-    if user&.authenticate(params[:session][:password])
-      log_in user
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user&.authenticate(params[:session][:password])
+      log_in @user
+      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
       # user_url(user)
-      redirect_to user
+      redirect_to @user
     else
       flash.now[:danger] = 'Invalid email / password combination'
       render 'new'
@@ -20,7 +20,8 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    # 　ログインしているときのみログアウトできる
+    log_out if logged_in?
     redirect_to root_url
     # flashを追加したい
   end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,13 +6,25 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  # signedメソッドー＞暗号化と複合化が可能なメソッド
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
   # ログインしている状態であれば、sessionのuser_idを保有している。それが、@current_userに格納されているのか、なければ追加する
   # cookieに保存さたユーザーIDをもとにユーザーの情報を取得するメソッド
   def current_user
-    return unless session[:user_id]
-
-    # ログインとログアウトで使用することからインスタンス変数としている
-    @current_user ||= User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user&.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
   end
 
   # userのログイン状態をtrue or false で判断したい
@@ -20,7 +32,16 @@ module SessionsHelper
     !current_user.nil?
   end
 
+  # 永続的セッションを破棄する
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
+  # 現在のユーザーをログアウトさせる
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
