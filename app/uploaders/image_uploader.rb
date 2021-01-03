70ac48a932a7d06a5d7b010c19a3ae9d28626189
harluz <harluz06@gmail.com>
2021-01-03 18:40:35 +0900

    Fixed image uploader

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,6 +4,21 @@ class ImageUploader < CarrierWave::Uploader::Base
   # リサイズしたり画像形式を変更するのに必要
   include CarrierWave::RMagick
 
+  # アップロードした画像の表示
+  # Override the directory where uploaded files will be stored.
+  # This is a sensible default for uploaders that are meant to be mounted:
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  # デフォルト画像の設定
+  # Provide a default URL as a default if there hasn't been a file uploaded:
+  def default_url(*_args)
+    #   For Rails 3.1+ asset pipeline compatibility:
+    ActionController::Base.helpers.asset_path('/images/' + [version_name, 'default.png'].compact.join('_'))
+    #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+  end
+
   # 画像の上限を640x480にする
   process resize_to_limit: [640, 480]
 
