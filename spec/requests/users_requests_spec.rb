56ddfafbfcb29682c1b03c867e18a7e598101f57
harluz <harluz06@gmail.com>
2021-01-03 18:43:10 +0900

    Add rspec

--- spec/requests/users_requests_spec.rb
+++ spec/requests/users_requests_spec.rb
@@ -3,14 +3,45 @@
 require 'rails_helper'
 
 RSpec.describe 'UsersRequests', type: :request do
-  before do
-    get signup_path
+  describe 'GET #index' do
+    subject { response }
+    # ユーザーのログインの有無を確認
+    describe 'before_action :logged_in_user' do
+      let!(:user) { FactoryBot.create(:user) }
+      # ユーザーがログインしている場合
+      context 'The user is logged in' do
+        before do
+          sign_in(user)
+          get users_path
+        end
+        it 'responds successfully' do
+          is_expected.to be_successful
+        end
+
+        it 'returns a 200 response' do
+          is_expected.to have_http_status(200)
+        end
+      end
+      # ユーザーがログインしていない場合
+      context 'The user is not logged in' do
+        before do
+          get users_path
+        end
+
+        it 'redirect to login url' do
+          is_expected.to redirect_to login_url
+        end
+
+        it 'returns a 302 response' do
+          is_expected.to have_http_status(302)
+        end
+      end
+    end
   end
 
-  # describe 'GET #index' do; end
-
   describe 'GET #show' do
-    before { get users_path, params: { user: create(:user) } }
+    let(:user) { FactoryBot.create(:user) }
+    before { get user_path user }
     subject { response }
 
     it 'responds successfully' do
@@ -36,7 +67,7 @@ RSpec.describe 'UsersRequests', type: :request do
   end
 
   describe 'POST #create' do
-    describe 'valid request' do
+    context 'valid request' do
       describe 'check users count' do
         it 'add a user' do
           expect do
@@ -63,7 +94,7 @@ RSpec.describe 'UsersRequests', type: :request do
       end
     end
 
-    describe 'invalid request' do
+    context 'invalid request' do
       describe 'check user count' do
         it 'does not add a user' do
           expect do
@@ -94,12 +125,278 @@ RSpec.describe 'UsersRequests', type: :request do
       end
     end
   end
+
+  describe 'GET #edit' do
+    subject { response }
+    # ユーザーのログインの有無を確認
+    describe 'before_action :logged_in_user' do
+      # ユーザーがログインしている場合
+      context 'The user is logged in' do
+        # editへのアクセス権限を確認する（ユーザーの正当性の確認）
+        describe 'before_action :correct_user' do
+          let!(:user) { FactoryBot.create(:user) }
+          let!(:other_user) { FactoryBot.create(:other_user) }
+          # 正しいユーザーの時
+          context 'When the corret user' do
+            before do
+              sign_in(user)
+              get edit_user_path(user)
+            end
+
+            it 'responds successfully' do
+              is_expected.to be_successful
+            end
+
+            it 'returns a 200 response' do
+              is_expected.to have_http_status(200)
+            end
+          end
+          # 正しくないユーザーの時
+          context 'When the incorrect user' do
+            before do
+              sign_in(other_user)
+              get edit_user_path(user)
+            end
+
+            it 'redirect to root url' do
+              is_expected.to redirect_to root_url
+            end
+
+            it 'returns a 302 response' do
+              is_expected.to have_http_status(302)
+            end
+          end
+        end
+      end
+
+      # ユーザーがログインしていない場合
+      context 'The user is not logged in' do
+        let(:user) { FactoryBot.create(:user) }
+        before do
+          get edit_user_path(user)
+        end
+
+        it 'redirect to login url' do
+          is_expected.to redirect_to login_url
+        end
+
+        it 'returns a 302 response' do
+          is_expected.to have_http_status(302)
+        end
+      end
+    end
+  end
+
+  describe 'PUT #update' do
+    # ユーザーのログインの有無を確認
+    describe 'before_action :logged_in_user' do
+      subject { response }
+      # ユーザーがログインしている場合
+      context 'The user is logged in' do
+        # updateへのアクセス権限を確認する（ユーザーの正当性の確認）
+        describe 'before_action :correct_user' do
+          let(:user) { FactoryBot.create(:user) }
+          let(:other_user) { FactoryBot.create(:other_user) }
+          # 正しいユーザーのとき
+          before { sign_in user }
+          context "when the correct user" do
+            # updateが成功するとき
+            context 'when editting is successful' do
+              before do
+                patch user_path(user), params: { user: {
+                  name: 'Foo Bar',
+                  email: 'foo@bar.com',
+                  password: '',
+                  password_confirmation: ''
+                } }
+              end
+
+              it 'redirect to user_path(user)' do
+                is_expected.to redirect_to user_path(user)
+              end
+
+              it 'returns a 302 response' do
+                is_expected.to have_http_status(302)
+              end
+            end
+
+            # updateが失敗するとき
+            context 'when editting fails' do
+              before do
+                patch user_path(user), params: { user: {
+                  name: 'Foo Bar',
+                  email: 'foo@bar',
+                  password: 'foo',
+                  password_confirmation: 'bar'
+                } }
+              end
+
+              it 'render edit' do
+                is_expected.to render_template('edit')
+              end
+
+              it 'returns a 200 response' do
+                is_expected.to have_http_status(200)
+              end
+            end
+          end
+
+          # 正しくないユーザーのとき
+          context "when the incorrect user" do
+            before { sign_in other_user }
+            # 正常な値を送信しても失敗する
+            context 'when editting is successful' do
+              before do
+                patch user_path(user), params: { user: {
+                  name: 'Foo Bar',
+                  email: 'foo@bar.com',
+                  password: '',
+                  password_confirmation: ''
+                } }
+              end
+
+              it 'redirect to root url' do
+                is_expected.to redirect_to root_url
+              end
+  
+              it 'returns a 302 response' do
+                is_expected.to have_http_status(302)
+              end
+            end
+
+            # 不正な値を送信しても失敗する
+            context 'when editting fails' do
+              before do
+                patch user_path(user), params: { user: {
+                  name: 'Foo Bar',
+                  email: 'foo@bar',
+                  password: 'foo',
+                  password_confirmation: 'bar'
+                } }
+              end
+
+              it 'redirect to root url' do
+                is_expected.to redirect_to root_url
+              end
+  
+              it 'returns a 302 response' do
+                is_expected.to have_http_status(302)
+              end
+            end
+          end
+        end
+      end
+      # ユーザーがログインしていない場合
+      context 'The user is not logged in' do
+        let(:user) { FactoryBot.create(:user) }
+        before do
+          patch user_path(user)
+        end
+
+        it 'redirect to login url' do
+          is_expected.to redirect_to login_url
+        end
+
+        it 'returns a 302 response' do
+          is_expected.to have_http_status(302)
+        end
+      end
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    # ユーザーのログインの有無を確認
+    describe 'before_action :logged_in_user' do
+      subject { response }
+      let!(:user) { FactoryBot.create(:user, :admin) }
+      let!(:other_user) { FactoryBot.create(:other_user) }
+      # ユーザーがログインしている場合
+      context 'The user is logged in' do
+        before do
+          sign_in(user)
+          delete user_path(other_user)
+        end
+        it 'responds successfully' do
+          is_expected.to redirect_to users_url
+        end
+
+        it 'returns a 200 response' do
+          is_expected.to have_http_status(302)
+        end
+      end
+      # ユーザーがログインしていない場合
+      context 'The user is not logged in' do
+        before { delete user_path(user) }
+
+        it 'redirect to login url' do
+          is_expected.to redirect_to login_url
+        end
+
+        it 'returns a 302 response' do
+          is_expected.to have_http_status(302)
+        end
+      end
+    end
+
+    # adminであるかを確認
+    describe 'before_action :admin_user' do
+      let!(:user) { FactoryBot.create(:user, :admin) }
+      let!(:other_user) { FactoryBot.create(:other_user) }
+      let!(:other_user2) { FactoryBot.create(:other_user) }
+      subject { response }
+      # アドミンである場合
+      context 'The user is admin' do
+        before do
+          sign_in(user)
+          delete user_path(other_user)
+        end
+        it 'responds successfully' do
+          is_expected.to redirect_to users_url
+        end
+
+        it 'returns a 200 response' do
+          is_expected.to have_http_status(302)
+        end
+      end
+
+      # アドミンでない場合
+      context 'The user is not admin' do
+        before do
+          sign_in(other_user)
+          delete user_path(other_user2)
+        end
+        it 'redirect to root url' do
+          is_expected.to redirect_to root_url
+        end
+
+        it 'returns a 302 response' do
+          is_expected.to have_http_status(302)
+        end
+      end
+    end
+
+    # ユーザー数の検証
+    describe "User count verification" do
+      let!(:user) { FactoryBot.create(:user, :admin) }
+      let!(:other_user) { FactoryBot.create(:other_user) }
+      before { sign_in user }
+      # deleteが成功するとき
+      context "When delete succeeds" do
+        it 'User count dectrases' do
+          expect do
+            delete user_path(other_user), params: { id: other_user.id }
+          end.to change(User, :count).by(-1)
+        end
+      end
+      
+      # deleteが失敗するとき
+      context "When delete fails" do
+        it 'User count does not change' do
+          expect do
+            delete user_path(user), params: { id: user.id }
+          end.to change(User, :count).by(0)
+        end
+      end
+    end
+  end
 end
-
-# describe 'GET #edit' do; end
-
-# describe 'PUT #update' do; end
-
-# describe 'DELETE #destroy' do; end
-
-# end
---
+++ spec/system/users_edits_system_spec.rb
@@ -0,0 +1,91 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'UsersEdits', type: :system do
+  describe 'About edit' do
+    let(:user) { FactoryBot.create(:user) }
+    before do
+      visit login_path
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: user.password
+      click_button 'Log in'
+      click_on 'Setting'
+    end
+    # 編集に成功する場合
+    context 'when editting is successful' do
+      before do
+        fill_in 'Name',	with: ''
+        fill_in 'Email',	with: 'valid@invalid.com'
+        fill_in 'Password',	with: 'foo'
+        fill_in 'Confirmation',	with: 'foo'
+        click_on 'Save changes'
+      end
+      # pathは正しいか
+      it 'path is correct' do
+        expect(current_path).to eq user_path(user)
+      end
+
+      # フラッシュメッセージは正しいか
+      it 'flash message is correct' do
+        expect(has_css?('.alert-danger')).to be_truthy
+      end
+
+      # 違うページにアクセスした時
+      context 'when accessing another page' do
+        before { visit root_url }
+        # フラッシュメッセージが消える
+        it 'is flash dissapear' do
+          expect(page).to_not have_selector('.alert-success', text: 'Profile updated')
+        end
+      end
+      
+      # ページをリロードした時
+      context 'when the page is reloaaded' do
+        before { visit current_path }
+        # フラッシュメッセージが消える
+        it 'is flash diasspear' do
+          expect(page).to_not have_selector('.alert-success', text: 'Profile updated')
+        end
+      end
+    end
+
+    # 編集に失敗する場合
+    context 'when editting fails' do
+      before do
+        fill_in 'Name',	with: ''
+        fill_in 'Email',	with: 'invalid@invalid'
+        fill_in 'Password',	with: 'foo'
+        fill_in 'Confirmation',	with: 'bar'
+        click_on 'Save changes'
+      end
+      # pathは正しいか
+      it 'path is correct' do
+        expect(current_path).to eq user_path(user)
+      end
+
+      # フラッシュメッセージは正しいか
+      it 'flash message is correct' do
+        expect(has_css?('.alert-danger')).to be_truthy
+      end
+
+      # 違うページにアクセスした時
+      context 'when accessing another page' do
+        before { visit root_url }
+        # フラッシュメッセージが消える
+        it 'is flash dissapear' do
+          expect(has_css?('.alert-danger')).to_not be_truthy
+        end
+      end
+      
+      # ページをリロードした時
+      context 'when the page is reloaaded' do
+        before { visit current_path }
+        # フラッシュメッセージが消える
+        it 'is flash diasspear' do
+          expect(has_css?('.alert-danger')).to_not be_truthy
+        end
+      end
+    end
+  end
+end
