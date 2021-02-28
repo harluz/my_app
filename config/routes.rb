c878be50036cba06c3d7e3dfdc8d695001ffde2b
harluz <harluz06@gmail.com>
2021-02-28 15:33:27 +0900

    Add order routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,18 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'orders/new'
+  get 'orders/create'
+  get 'orders/edit'
+  get 'orders/update'
+  get 'orders/destroy'
   get 'password_resets/new'
   get 'password_resets/edit'
   get 'sessions/new'
   root 'static_pages#home'
-  # get '/log_in'  => 'static_pages#log_in'
-  # get '/log_out' => 'static_pages#log_out'
   get '/about'   => 'static_pages#about'
   get '/profile' => 'static_pages#profile'
-  # get '/my_list' => 'static_pages#my_list'
-  # get '/suggest' => 'static_pages#suggest'
-  # get '/seek'    => 'static_pages#seek'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # ログイン
@@ -27,7 +28,8 @@ Rails.application.routes.draw do
   resources :password_resets, only: %i[new create edit update]
 
   # 投稿機能
-  # get '/posts' => 'posts#new'
-  # delete '/posts' => 'posts#destroy'
   resources :posts, only: %i[new create edit update destroy]
+
+  # 注文機能
+  resources :orders, only: %i[new create edit update destroy]
 end
